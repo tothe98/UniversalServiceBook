c5a369d1959de79208b9f5f311fe8508793a582d
Daniel Molnar <molnard.work@gmail.com>
2022-10-12 08:05:50 +0200

    I created the client folder & I created the react app.

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# Universal Service Book Client Side
---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <React.Fragment>
+        <h1>Hello world!</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
