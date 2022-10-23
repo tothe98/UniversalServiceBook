e23711d8c878ff6ef28d868967d30e7c06bc1f9d
Daniel Molnar <molnard.work@gmail.com>
2022-10-23 16:09:25 +0200

    I did the home&settings&garage page structures and these pages are responsive.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,28 @@
+import { Container, createTheme, ThemeProvider } from '@mui/material';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import React, { Component } from 'react';
+import Home from './pages/Home'
+import theme from './themes/theme'
+import Garage from './pages/Garage';
+import Settings from './pages/Settings';
+
 function App() {
   return (
-    <React.Fragment>
-        <h1>Hello world!</h1>
-    </React.Fragment>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="l">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/garage" element={<Garage />}></Route>
+            <Route path="/settings" element={<Settings />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/themes/theme.js
@@ -0,0 +1,18 @@
+import { createTheme } from "@mui/material";
+
+export default createTheme({
+    palette: {
+        dark: {
+            main: '#17181D'
+        },
+        lightdark: {
+            main: '#22222C'
+        },
+        yellow: {
+            main: '#FFF500'
+        },
+        orange: {
+            main: '#FFC412'
+        }
+    }
+})
