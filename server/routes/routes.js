56e852fef46704eecbcbf33f12b7a16ac9df01ba
Tóth Erik <totherikjanos@gmail.com>
2022-11-15 12:34:55 +0100

    Swagger init

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 const { signup, signin } = require('../controllers/AuthController')
 const { addNewVehicle } = require('../controllers/VehicleController')
-const {authenticateToken, authenticateAdmin, authenticateWorkshop} = require('../core/Auth')
+const { authenticateToken, authenticateAdmin, authenticateWorkshop } = require('../core/Auth')
 
 router.get('/', (req, res) => {
     res.status(200).json(
@@ -29,9 +29,39 @@ router.get('/', (req, res) => {
         }
     )
 })
-
+/**
+ * @swagger
+ * /api/v1/signup:
+ *   post:
+ *     summary: Felhasználó regisztrálása
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               fname:
+ *                 type: string
+ *                 description: Felhasználó keresztneve.
+ *                 example: Erik
+ *               lname:
+ *                  type: string
+ *                  description: Felhasználó vezetékneve.
+ *                  example: Molnár
+ *               email:
+ *                  type: string
+ *                  description: Felhasználó email címe.
+ *                  example: erik.molnar@gmail.com
+ *               password:
+ *                  type: string
+ *                  description: Felhasználó jelszava.
+ *     responses:
+ *       201:
+ *         'Sikeres regisztráció'
+*/
 router.post('/signup', signup)
 router.post('/signin', signin)
-router.post('/addNewVehicle', authenticateToken ,addNewVehicle)
+router.post('/addNewVehicle', authenticateToken, addNewVehicle)
 
 module.exports = router
