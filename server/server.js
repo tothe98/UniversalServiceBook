db279616c06241eec402d901f6324aaf3608e7fb
Daniel Molnar <molnard.work@gmail.com>
2022-11-16 13:25:26 +0100

    I did the authentication checks

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express()
 app.use(express.json())
 app.use(
     cors({
-        origin: ['http://localhost:8080'],
+        origin: ['http://localhost:3000'],
         methods: ['GET', 'POST', 'PATH', 'DELETE'],
         credentials: true
     })
