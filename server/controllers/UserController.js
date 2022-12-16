53e4cf8cdabe0c6c1e511a8248c9475b6fecd9d9
Tóth Erik <totherikjanos@gmail.com>
2022-12-16 18:52:12 +0100

    #18 - Backend - MailSend
    +Email confirmation + ForgotPasswor

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const sendEmail = require("../core/Mailer")
+const confirmationEmail = require('../core/MailViews')
 require('../models/UserModel')
 require('../models/PictureModel')
 
@@ -57,4 +60,55 @@ exports.updateUser = async (req, res) => {
         return res.status(400).json({ message: 'error', data: { error: err } })
     }
     res.status(200).json({ message: 'OK', data: {} })
+}
+
+exports.forgotPassword = async (req, res) => {
+    const { email } = req.body
+    if (!email) {
+        return res.status(422).json({ message: 'EmailIsEmpty', data: {} })
+    }
+    try {
+        const User = mongoose.model('UserInfo')
+        const findUser = await User.findOne({ email: email })
+        if (findUser) {
+            const token = jwt.sign({ userId: findUser._id, email: findUser.email }, process.env.JWT_SECRET, { expiresIn: '15m' })
+
+            await sendEmail(findUser.email, "Elfelejtett jelszó",
+                confirmationEmail(`http://127.0.0.1:8080/api/v1/newPassword/${token}`)
+            )
+            return res.status(200).json({ message: 'EmailIsSent', data: {} })
+        } else {
+            return res.status(404).json({ message: 'UserIsNotFound', data: { error } })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: 'error', data: { error } })
+    }
+}
+
+exports.newPassword = async (req, res) => {
+    const { token, password, cpassword } = req.body
+
+    if (!token || !password || !cpassword) {
+        return res.status(422).json({ message: 'FieldsMustBeField', data: {} })
+    }
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+            return res.status(409).json({ message: 'TokenIsExpired', data: [] })
+        }
+        try {
+            if (password === cpassword) {
+                const User = mongoose.model('UserInfo')
+                const updateUser = await User.findOne({ _id: decoded.userId, email: decoded.email })
+                const salt = bcrypt.genSaltSync(10)
+                const updatePsw = bcrypt.hashSync(password, salt)
+                updateUser.password = updatePsw
+                await updateUser.save()
+                return res.status(201).json({ message: 'success', data: {} })
+            } else {
+                return res.status(409).json({ message: 'PasswordIsNotEqual', data: [] })
+            }
+        } catch (error) {
+            return res.status(500).json({ message: 'error', data: { error } })
+        }
+    })
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const { signup, signin } = require('../controllers/AuthController')
+
+const { signup, signin, confirmEmail, isValidToken } = require('../controllers/AuthController')
 const { addVehicle, getVehicles, getVehicle, updateVehicle } = require('../controllers/VehicleController')
-const { getUser, updateUser } = require('../controllers/UserController')
+const { getUser, updateUser, forgotPassword, newPassword } = require('../controllers/UserController')
 const { authenticateToken, authenticateAdmin, authenticateWorkshop } = require('../core/Auth')
 const {
     getManufactures,
@@ -25,11 +26,15 @@ const {
 //AuthController
 router.post('/signup', signup)
 router.post('/signin', signin)
+router.get('/emailConfirmation/:token', confirmEmail)
+router.get('/isValidToken/:token', isValidToken)
 router.get('/isLoggedIn', authenticateToken, (req, res) => { res.status(200).json({ message: 'ok', data: {} }) })
 
 //UserController
 router.get('/getUserData', authenticateToken, getUser)
 router.put('/updateUser', authenticateToken, updateUser)
+router.post('/forgotPassword', forgotPassword)
+router.post('/newPassword', newPassword)
 
 //VehicleParameterController
 router.post('/getManufactures', authenticateToken, getManufactures)
